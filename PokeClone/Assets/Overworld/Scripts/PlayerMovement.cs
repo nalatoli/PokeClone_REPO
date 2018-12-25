1c4032270ace7aaf38e470977a9f5820083741ad
Norildeen Alatoli <norilalatoli@gmail.com>
2018-12-25 12:28:15 -0500

    Player Movement
    A test environment for player movement has been created. The player can now walk and run along the map.

---
+++ PokeClone/Assets/Overworld/Scripts/PlayerMovement.cs
@@ -0,0 +1,53 @@
+﻿/* Essential Namespaces */
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/***************************************************************************************** 
+ * The 'PlayerMovement' class controls the movement of the player. The player can walk,
+ * run, bike, and surf in the four cardinal directions (North, South, East, and West). 
+ * The direction of motion is determined by the user key input at the start of every
+ * overworld frame. The type of motion is determined by the state of the player and 
+ * the key input at the given overworld frame.
+ *  
+ ****************************************************************************************/
+public class PlayerMovement : MonoBehaviour
+{
+    /* Adjustable Parameters */
+    public float speed;
+    public bool isControllable;
+    
+    /* Public Parameters */
+    public Vector3 dir;
+    public bool isMovable;
+
+    /* Private Parameter */
+    private Vector3 targetPos;
+
+    void Start ()
+    {
+        /* Initialize Parameters */
+        dir = Vector3.down;
+        targetPos = transform.position;
+    }
+
+    void Update ()
+    {
+        /* Determine if Player is Movable */
+        isMovable = (transform.position == targetPos) && isControllable;
+        // ...
+      
+        /* If Player is Controllable */
+        if(isMovable)
+        {
+            /* Get Direction of Player */
+            if      (Input.GetKey(KeyCode.W)) { dir = Vector3.up;       targetPos += dir; }
+            else if (Input.GetKey(KeyCode.D)) { dir = Vector3.right;    targetPos += dir; }
+            else if (Input.GetKey(KeyCode.S)) { dir = Vector3.down;     targetPos += dir; }
+            else if (Input.GetKey(KeyCode.A)) { dir = Vector3.left;     targetPos += dir; }
+        }
+
+        /* Move Player */
+        transform.position = Vector3.MoveTowards(transform.position, targetPos, Time.deltaTime * speed);
+    }
+}
---
+++ PokeClone/Assets/Overworld/Scripts/StateController.cs
@@ -0,0 +1,72 @@
+﻿/* Essential Namespaces */
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+/***************************************************************************************** 
+ * The 'StateController' class controls the state of the player. The state constitutes
+ * the speed and animations of the player. The speed is adjustable publically.
+ * The speed and animations will NOT apply to the player until the player is MOVABLE.
+ *  
+ ****************************************************************************************/
+public class StateController : MonoBehaviour
+{
+    /* Enumeration of States */
+    public enum State {
+        idle,
+        walking,
+        running,
+        biking,
+        surfing
+    };
+
+    /* Direction -> Animation Trigger */
+    Dictionary<Vector3, string> dir2trigger = new Dictionary<Vector3, string>() {
+        {Vector3.up,    "goUp" },
+        {Vector3.right, "goRight" },
+        {Vector3.down,  "goDown" },
+        {Vector3.left,  "goLeft" }
+    };
+
+    /* State -> (Animation) Speed */
+    Dictionary<State, float> state2speed = new Dictionary<State, float>() {
+        {State.idle,    0 },
+        {State.walking, 3 },
+        {State.running, 6 },
+        {State.biking,  8 },  
+        {State.surfing, 8 }
+    };
+
+    /* Private Parameters */
+    private State state;
+    private Animator anim;
+    private PlayerMovement player;
+
+
+    void Start()
+    {
+        /* Initialize Parameters */
+        state = State.idle;
+        anim = GetComponent<Animator>();
+        player = GetComponent<PlayerMovement>();
+    }
+
+    void Update()
+    {
+        /* Get State of Player */
+        if (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.A))
+            state = Input.GetKey(KeyCode.LeftShift) ? State.running : State.walking;
+
+        else
+            state = State.idle;
+
+
+        /* If Player is Movable */
+        if(player.isMovable)
+        {
+            /* Update Animation and Speed of Player */
+            anim.SetFloat("Speed", state2speed[state]);
+            anim.SetTrigger(dir2trigger[player.dir]);
+            player.speed = state2speed[state];
+        }
+    }
+}
